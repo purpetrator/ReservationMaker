5f168b2072e9b74e831fa9e7a153015bb85b9f74
Ana Chernov <anastasia.chernov@gmail.com>
2019-11-20 10:57:59 -0500

    Uploading app to github

---
+++ server.js
@@ -0,0 +1,109 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// =============================================================
+var reservations = [
+  {
+    routeName: "john",
+    name: "John",
+    phone: "215-222-2222",
+    email: "john@email.com",
+    unique_id: "1"
+  },
+  {
+    routeName: "susan",
+    name: "Susan",
+    phone: "215-333-3333",
+    email: "susan@email.com",
+    unique_id: "2"
+  },
+  {
+    routeName: "bob",
+    name: "Bob",
+    phone: "215-444-4444",
+    email: "bob@email.com",
+    unique_id: "3"
+  }
+];
+
+var waitlist = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Displays all reservations
+app.get("/api/reservations", function(req, res) {
+  return res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitlist);
+});
+
+app.get("/api/reservations/:reservation", function(req, res) {
+  var chosen = req.params.reservation;
+
+  console.log(chosen);
+
+  for (var i = 0; i < reservations.length; i++) {
+    if (chosen === reservations[i].routeName) {
+      return res.json(reservations[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Reservation - takes in JSON input
+app.post("/api/reservations", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newReservation = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newReservation
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newReservation.routeName = newReservation.name
+    .replace(/\s+/g, "")
+    .toLowerCase();
+
+  console.log(newReservation);
+
+  res.json(newReservation);
+
+  //   for (var i = 0; i < reservations.length; i++) {
+  if (reservations.length < 5) {
+    reservations.push(newReservation);
+  } else {
+    waitlist.push(newReservation);
+  }
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
